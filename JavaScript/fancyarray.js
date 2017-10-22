390bc616c2bbb58a058b6b724adea9f2db89353b
Ghezal Sherdil <ghezal.sherdil@gmail.com>
2017-10-22 19:06:58 -0400

    new code

---
+++ JavaScript/fancyarray.js
@@ -0,0 +1,12 @@
+var names = ["James", "Jill", "Jane", "Jack"];
+
+function symbol(s) {
+  for (var i =0; i < names.length; i++){
+      // console.log(i + " -> " + names[i]);
+      console.log(i + " " + s + " " + names[i]);
+  }
+}
+symbol("=>")
+symbol("->")
+symbol("::")
+symbol("----")
