3dcf743d1747b2102e0c25e6680a0557d55cc356
Ghezal Sherdil <ghezal.sherdil@gmail.com>
2017-10-22 15:53:43 -0400

    new file

---
+++ JavaScript/daysuntilbday.js
@@ -0,0 +1,19 @@
+var daysUntilMyBirthday = 600;
+while (daysUntilMyBirthday >=30){
+  console.log(daysUntilMyBirthday+" days until my birthday. Such a long time... :(");
+  daysUntilMyBirthday = daysUntilMyBirthday -1;
+}
+console.log("daysUntilMyBirthday: "+ daysUntilMyBirthday);
+while (daysUntilMyBirthday >= 5) {
+  console.log(daysUntilMyBirthday + " days until my birthday.Its ALMOST my birthday!");
+  daysUntilMyBirthday = daysUntilMyBirthday -1;
+}
+console.log("daysUntilMyBirthday: "+ daysUntilMyBirthday);
+while (daysUntilMyBirthday > 0 ) {
+  console.log(daysUntilMyBirthday + " days until my birthday. YAY! ITS ALMOST MY BIRTHDAY!!!");
+  daysUntilMyBirthday = daysUntilMyBirthday -1;
+}
+console.log("daysUntilMyBirthday: "+ daysUntilMyBirthday);
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•");
