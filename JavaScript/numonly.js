6a47c2480a40f30a2ad6ed1dfabc59c7de0a5f7f
Ghezal Sherdil <ghezal.sherdil@gmail.com>
2017-10-23 22:35:25 -0400

    new code

--- JavaScript/numonly.js
+++ JavaScript/numonly.js
@@ -1,15 +1,11 @@
-var numbersOnly = function (list){
-
-var placeHolder = [];
-
-for(var i = 0; i < list.length; i ++){
-  if(typeof list[i] == "number"){
-    placeHolder.push(list[i]);
-
+var onlyNumbers = function (list){
+  var container = []
+  for (var i = 0; i < list.length; i++){
+    if (typeof list[i] == "number"){
+      container.push(list[i]);
+    }
   }
+  return container
 }
-  return placeHolder;
-}
-
-  var test = numbersOnly([23, "apples", "bannana", 123987, "true", 843, -9843, "hopethisworks"])
-  console.log(test);
+var test =onlyNumbers([12,"hello",473289, -9834,"ok",-984])
+console.log(test);
