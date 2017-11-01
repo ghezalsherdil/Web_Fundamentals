f1fcc5e8d01630fbc221cfd76a89f2eff648b214
Ghezal Sherdil <ghezal.sherdil@gmail.com>
2017-10-31 23:11:17 -0400

    new code

---
+++ Python/filter-by-type.py
@@ -0,0 +1,20 @@
+#If the integer is greater than or equal to 100, print "That's a big number!" If the integer is less than 100, print "That's a small number"
+bI = 455
+if bI >= 100:
+    print "Thats a big number!"
+elif x < 100:
+    print "Thats a small number"
+
+#If the string is greater than or equal to 50 characters print "Long sentence." If the string is shorter than 50 characters print "Short sentence."
+sS= "Tell me and I forget. Teach me and I remember. Invole me and I learn."
+if len(sS) >= 50:
+    print "Long sentence"
+elif len(sS) < 50:
+    print "short"
+
+#If the length of the list is greater than or equal to 10 print "Big list!" If the list has fewer than 10 values print "Short list."
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+if len(lL) >= 10:
+    print "Big list!"
+elif len(lL) < 10:
+    print "Short list"
