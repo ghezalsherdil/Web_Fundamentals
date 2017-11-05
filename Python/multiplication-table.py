74d2c673410c126931958739b20caf7f361da2bd
Ghezal Sherdil <ghezal.sherdil@gmail.com>
2017-11-05 15:01:30 -0500

    new code

--- Python/multiplication-table.py
+++ Python/multiplication-table.py
@@ -1,11 +1,11 @@
 print ("\t\t\t\tMultiplication Table\n")
 
 for i in range(1,13):
-    print (i, end='\t')
+    print ' ', i,
 print ()
-print('_________________________________________________________\n')
+print '_________________________________________________________\n'
 
 for j in range(1,13):
     for k in range(1,13):
-        print(j*K, end='\t')
-    print('\n)
+        print ' ', j * k,
+    print '\n'
