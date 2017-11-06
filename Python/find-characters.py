5066d456c763d3632a7e4e20748c6d194659e06d
Ghezal Sherdil <ghezal.sherdil@gmail.com>
2017-11-06 18:38:22 -0500

    new code

--- Python/find-characters.py
+++ Python/find-characters.py
@@ -1,3 +1,17 @@
+'''Assignment: Find Characters
+Write a program that takes a list of strings and a string containing a single character, and prints a new list of all the strings containing that character.
+
+Here's an example:
+
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']
+Copy
+Hint: how many loops will you need to complete this task?
+'''
+
 colors = ['red', 'green', 'pink', 'black', 'brown', 'orange', 'yellow']
 letter = set('e')
 
