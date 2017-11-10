362a934134c0883ed4dbafa403686fa9c1843620
Sherdil <gsherdil@gssla17101427.ndc.nasa.gov>
2017-11-09 20:21:01 -0500

    new code

--- Python/Flask/form_test/server.py
+++ Python/Flask/form_test/server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 # our index route will handle rendering our form
+app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
+# routing rules and rest of server.py below
 @app.route('/')
 def index():
   return render_template("index.html")
@@ -11,8 +13,15 @@ def create_user():
    print "Got Post Info"
    # we'll talk about the following two lines after we learn a little more
    # about forms
-   name = request.form['name']
-   email = request.form['email']
+   session['name'] = request.form['name']
+   session['email'] = request.form['email']
    # redirects back to the '/' route
-   return redirect('/users')
+   return "Got user info"
+   return request.form['name']
+   return redirect('/show')
+
+@app.route('/show')
+def show_user():
+  return render_template('user.html', name=session['name'], email=session['email'])
+
 app.run(debug=True) # run our server
